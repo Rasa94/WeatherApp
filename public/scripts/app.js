7dbe4d15b1d6fe7505a5d474abf3cea2cd294781
Rasa94 <radivojejovanovic94@gmail.com>
2020-10-21 15:48:56 +0200

    Fixed bugs and updated style

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,45 +7,54 @@ let conditions = document.getElementById("cond");
 let icon = document.getElementById("icon");
 let future = document.getElementById("future");
 let futureEl = document.querySelectorAll(".futureEl");
+let futureDate = document.querySelectorAll(".futureDate");
 
 
 
 
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let key = "22df6733b2539ed71b9f619e1a7f16c3";
 
 let dateFormat = d => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
+    dateInfo.innerHTML =  `${day} ${date} ${month} ${year}`
+}
+
+let futureDateFormat = d => {
+    let day = d.getDay();
+    futureDate.forEach(element => {
+        day == 6 ? day = 0 : day += 1;
+        console.log(day)
+        element.innerHTML = days[day];
+    })
 }
 
 let weatherIcon = (el, time, location) => {
     if (el >= 200 && el <= 232) {
-        location.innerHTML += `<img src="/icons/thunderstorm.png">`
+        location.innerHTML += `<img src="./icons/thunderstorm.png">`
     } else if (el >= 300 && el <= 321) {
-        location.innerHTML += `<img src="/icons/drizzle.png">`;
+        location.innerHTML += `<img src="./icons/drizzle.png">`;
         console.log('Drizzle');
     } else if (el >= 500 && el <= 531) {
         console.log('Rain');
-        location.innerHTML += `<img src="/icons/rain.png">`;
+        location.innerHTML += `<img src="./icons/rain.png">`;
     } else if (el >= 600 && el <= 622) {
         console.log('Snow');
-        location.innerHTML += `<img src="/icons/snow.png">`;
+        location.innerHTML += `<img src="./icons/snow.png">`;
     } else if (el >= 701 && el <= 781){
         console.log('Atmosphere');
         location.innerHTML += ``;
     } else if (el == 800) {
         console.log('Clear');
-        time == "n" ? location.innerHTML += `<img src="/icons/clearNight.png">` : location.innerHTML += `<img src="/icons/clearDay.png">`;
+        time == "n" ? location.innerHTML += `<img src="./icons/clearNight.png">` : location.innerHTML += `<img src="./icons/clearDay.png">`;
     } else if (el >= 801 && el <= 804) {
         console.log('Clouds');
-        time == "n"  ? location.innerHTML += `<img src="/icons/cloudNight.png">` : location.innerHTML += `<img src="/icons/cloudDay.png">`;
+        time == "n"  ? location.innerHTML += `<img src="./icons/cloudNight.png">` : location.innerHTML += `<img src="./icons/cloudDay.png">`;
     }
 }
 
@@ -60,16 +69,17 @@ let getData = (name) => {
             element.innerHTML = "";
         });
         let dateData = new Date();
-        console.log(weather.list[0].sys.pod)
         console.log(weather);
         cityInfo.innerHTML = `${weather.city.name}, ${weather.city.country}`;
-        dateInfo.innerHTML = dateFormat(dateData); 
+        dateFormat(dateData); 
+        futureDateFormat(dateData);
         temperature.innerHTML = `${Math.round(weather.list[0].main.temp)}°c`;
         conditions.innerHTML =  weather.list[0].weather[0].main;
         weatherIcon(weather.list[0].weather[0].id, weather.list[0].sys.pod, icon)
         let list = weather.list
         let futureTemp = [];
         let futureId = [];
+        console.log(dateData.getDay())
 
         list.forEach((element, i) => {
             let el = element.dt_txt;
@@ -83,7 +93,6 @@ let getData = (name) => {
         futureEl.forEach((element, i) => {
             weatherIcon(futureId[i], 15, element)
             element.innerHTML += `<h2>${Math.round(futureTemp[i])}°c</h2>`;
-            console.log(futureTemp[i])
         });
     })
 }
