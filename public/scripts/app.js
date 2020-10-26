86932618f4a9635049c5a1391a948939e3d56515
Rasa94 <radivojejovanovic94@gmail.com>
2020-10-26 02:59:27 +0100

    Code refactoring

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,11 +5,9 @@ let dateInfo = document.getElementById("date");
 let temperature = document.getElementById("temp");
 let conditions = document.getElementById("cond");
 let icon = document.getElementById("icon");
-let future = document.getElementById("future");
+let futureForecastGroup = document.getElementById("futureForecastGroup");
 let futureEl = document.querySelectorAll(".futureEl");
 let futureDate = document.querySelectorAll(".futureDate");
-
-
 
 
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -29,7 +27,6 @@ let futureDateFormat = d => {
     let day = d.getDay();
     futureDate.forEach(element => {
         day == 6 ? day = 0 : day += 1;
-        console.log(day)
         element.innerHTML = days[day];
     })
 }
@@ -38,22 +35,22 @@ let weatherIcon = (el, time, location) => {
     if (el >= 200 && el <= 232) {
         location.innerHTML += `<img src="./icons/thunderstorm.png">`
     } else if (el >= 300 && el <= 321) {
+        // Drizzle
         location.innerHTML += `<img src="./icons/drizzle.png">`;
-        console.log('Drizzle');
     } else if (el >= 500 && el <= 531) {
-        console.log('Rain');
+        // Rain
         location.innerHTML += `<img src="./icons/rain.png">`;
     } else if (el >= 600 && el <= 622) {
-        console.log('Snow');
+        // Snow
         location.innerHTML += `<img src="./icons/snow.png">`;
     } else if (el >= 701 && el <= 781){
-        console.log('Atmosphere');
+        // Atmosphere
         location.innerHTML += ``;
     } else if (el == 800) {
-        console.log('Clear');
+        // Clear
         time == "n" ? location.innerHTML += `<img src="./icons/clearNight.png">` : location.innerHTML += `<img src="./icons/clearDay.png">`;
     } else if (el >= 801 && el <= 804) {
-        console.log('Clouds');
+        // Clouds
         time == "n"  ? location.innerHTML += `<img src="./icons/cloudNight.png">` : location.innerHTML += `<img src="./icons/cloudDay.png">`;
     }
 }
@@ -64,22 +61,27 @@ let getData = (name) => {
         return weather.json();
     })
     .then(weather => {
+        // Clear page
         icon.innerHTML = "";
         futureEl.forEach(element => {
             element.innerHTML = "";
         });
+
         let dateData = new Date();
-        console.log(weather);
+
+        // Render info
+            // Today's temp
         cityInfo.innerHTML = `${weather.city.name}, ${weather.city.country}`;
         dateFormat(dateData); 
         futureDateFormat(dateData);
         temperature.innerHTML = `${Math.round(weather.list[0].main.temp)}°c`;
         conditions.innerHTML =  weather.list[0].weather[0].main;
         weatherIcon(weather.list[0].weather[0].id, weather.list[0].sys.pod, icon)
+
+            // Temperature for the next 4 days
         let list = weather.list
         let futureTemp = [];
         let futureId = [];
-        console.log(dateData.getDay())
 
         list.forEach((element, i) => {
             let el = element.dt_txt;
@@ -90,6 +92,7 @@ let getData = (name) => {
             }
 
         });
+        
         futureEl.forEach((element, i) => {
             weatherIcon(futureId[i], 15, element)
             element.innerHTML += `<h2>${Math.round(futureTemp[i])}°c</h2>`;
@@ -102,6 +105,5 @@ let setQuery = (e) => {
         getData(searchInput.value)
     }
 }
-
 searchInput.addEventListener('keypress', setQuery)
 
